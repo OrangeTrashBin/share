101fa0f12236de6b3a8b2da3b0652fb4972bcdfc
OrangeTrashBin <113816763+OrangeTrashBin@users.noreply.github.com>
2023-02-13 16:17:41 -0800

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,10 @@
+def generator(state):
+  if check_terminal(state):
+    return state
+  else generator(random_move(state))
+
+def check_terminal(state);
+  return
+
+def random_move(state);
+  return
